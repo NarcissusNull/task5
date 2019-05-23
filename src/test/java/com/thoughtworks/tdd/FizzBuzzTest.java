82c77cdaac71f30175a56f82d72aa99ca771127b
Narcissus <1169714441@qq.com>
2019-05-23 18:23:41 +0800

    create should_print_Buzz() and refactor fizzBuzz() ,and make test pass by using simplest code

--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -18,10 +18,16 @@ public class FizzBuzzTest {
     @Test
     void should_print_Fizz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        Random random = new Random();
-        int multiple = random.nextInt(40);
-        String result = fizzBuzz.fizzBuzz(3 * multiple);
+        String result = fizzBuzz.fizzBuzz(48);
 
         assertEquals("Fizz", result);
+    }
+
+    @Test
+    void should_print_Buzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.fizzBuzz(10);
+
+        assertEquals("Buzz", result);
     }
 }
